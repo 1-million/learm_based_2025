6a374988eae59f4f2a1414896507a38d405daa4b
A56 <593747881@qq.com>
2025-06-01 15:08:04 +0800

    init

---
+++ src/test/java/com/qr/based/BasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qr.based;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
